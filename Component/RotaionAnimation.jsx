7da680ad393f3fc3fe939a244683452bb770f053
pratik <pratikpatre2019@gmail.com>
2022-02-08 21:18:58 +0530

    Prespective Animation

---
+++ Component/RotaionAnimation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function RotaionAnimation() {
+  return (
+    <View>
+      <Text> Rotation Animation using Moti </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
